b0d973ec4c5229625d35f859e73e53076d743376
asrivas3 <asrivas3@ncsu.edu>
2018-10-07 02:50:07 -0400

    front changes

--- MyApplication/app/src/main/java/com/example/abhishek/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/abhishek/myapplication/MainActivity.java
@@ -10,10 +10,15 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -21,11 +26,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 //import org.json.*;
 
@@ -77,85 +86,165 @@ public class MainActivity extends AppCompatActivity{
 
         HttpURLConnection urlConnection = null;
 
-    if(eventsList.size() == 0){
-     Toast.makeText(this, "No events found to schedule the travel.",
-             Toast.LENGTH_LONG).show();
-        Log.d("ANALWAY","123");
-     }
-     else if(eventsList.get(0).get(2).length() == 0){
-         Toast.makeText(this, "No location saved for this event.",
+        if(eventsList.size() == 0){
+         Toast.makeText(this, "No events found to schedule the travel.",
                  Toast.LENGTH_LONG).show();
-       Log.d("ANALWAY","123");
-     }
-     else{
-        Log.d("ANALWAY","123");
+//            Log.d("ANALWAY","123");
+         }
+         else if(eventsList.get(0).get(2).length() == 0){
+             Toast.makeText(this, "No location saved for this event.",
+                     Toast.LENGTH_LONG).show();
+//           Log.d("ANALWAY","123");
+         }
+         else{
 
-//        Log.d("ASRI","Count: "+eventsList.size());
-//        Log.d("ASRI","Title: "+eventsList.get(0).get(0));
-//        Log.d("ASRI","Time: "+eventsList.get(0).get(1));
-//        Log.d("ASRI","Location: "+eventsList.get(2).get(0));
-//        Log.d("ASRI","Location: "+eventsList.get(0).get(2));
-//        int tot_len = 0;
-        String curloc=null;
-        try {
-            /*try {
-                locmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
-                location_net = locmgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                location_gps = locmgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if (location_gps == null) {
-                    if (location_net == null) {
-                        Toast.makeText(this, "Location not Available", Toast.LENGTH_SHORT).show();
+    //        Log.d("ASRI","Count: "+eventsList.size());
+    //        Log.d("ASRI","Title: "+eventsList.get(0).get(0));
+    //        Log.d("ASRI","Time: "+eventsList.get(0).get(1));
+    //        Log.d("ASRI","Location: "+eventsList.get(2).get(0));
+    //        Log.d("ASRI","Location: "+eventsList.get(0).get(2));
+    //        int tot_len = 0;
+            String curloc=null;
+            try {
+                /*try {
+                    locmgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    location_net = locmgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    location_gps = locmgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location_gps == null) {
+                        if (location_net == null) {
+                            Toast.makeText(this, "Location not Available", Toast.LENGTH_SHORT).show();
+                        } else {
+                            location = location_net;
+                        }
                     } else {
-                        location = location_net;
+                        location = location_gps;
                     }
-                } else {
-                    location = location_gps;
+                    curloc = String.valueOf(location.getLatitude()) + "," + String.valueOf(location.getLongitude());
                 }
-                curloc = String.valueOf(location.getLatitude()) + "," + String.valueOf(location.getLongitude());
+                catch(SecurityException e){e.printStackTrace();}*/
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                String Url = "https://maps.googleapis.com/maps/api/directions/json?origin=Kings+Court+Raleigh&destination=" + eventsList.get(0).get(2) + "&key=AIzaSyBkLli9Te539Uob6HodeOgR-bD83JCrRcg&mode=transit&arrival_time=" + eventsList.get(0).get(1);
+                //String Url = "https://maps.googleapis.com/maps/api/directions/json?origin=Kings+Court+Raleigh&destination=" + eventsList.get(0).get(2) + "&key=AIzaSyBkLli9Te539Uob6HodeOgR-bD83JCrRcg&mode=transit&arrival_time=";
+
+                URL url = new URL(Url);
+                Log.d("URL ", String.valueOf(Url));
+                urlConnection = (HttpURLConnection) url.openConnection();
+                //Log.d("URL ", String.valueOf(urlConnection));
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader bR = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+
+                StringBuilder responseStrBuilder = new StringBuilder();
+                while ((line = bR.readLine()) != null) {
+
+                    responseStrBuilder.append(line);
+                }
+                in.close();
+
+                JSONObject result = new JSONObject(responseStrBuilder.toString());
+
+                List<JSONObject> lst = getTravelDetails(result);
+                Map<String,List<String>> map = processingJSON(lst);
+
+                Log.d("ASRI", String.valueOf(lst));
+                TextView showTitle = findViewById(R.id.showTitle);
+                String temp1 = eventsList.get(0).get(2);
+                temp1 = temp1.replace("+"," ");
+                showTitle.setText(eventsList.get(0).get(0));
+                TextView showTime = findViewById(R.id.showTime);
+                Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+                cal.setTimeInMillis(Long.parseLong(eventsList.get(0).get(1)) * 1000L);
+                android.text.format.DateFormat df = new android.text.format.DateFormat();
+                showTime.setText(df.format("dd/MM/yyyy hh:mm:ss", cal).toString());
+                TextView showLoc = findViewById(R.id.showLoc);
+                showLoc.setText(temp1);
+
+
+                TextView showHops = findViewById(R.id.showHops);
+                showHops.setText("# Hops: "+map.get("arrivalStop").size());
+
+                TextView startTime = findViewById(R.id.startTime);
+                startTime.setText(map.get("arrivalTime").get(0));
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                urlConnection.disconnect();
             }
-            catch(SecurityException e){e.printStackTrace();}*/
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            String Url = "https://maps.googleapis.com/maps/api/directions/json?origin=Kings+Court+Raleigh&destination=" + eventsList.get(0).get(2) + "&key=AIzaSyBkLli9Te539Uob6HodeOgR-bD83JCrRcg&mode=transit&arrival_time=" + eventsList.get(0).get(1);
-            //String Url = "https://maps.googleapis.com/maps/api/directions/json?origin=Kings+Court+Raleigh&destination=" + eventsList.get(0).get(2) + "&key=AIzaSyBkLli9Te539Uob6HodeOgR-bD83JCrRcg&mode=transit&arrival_time=";
-
-            URL url = new URL(Url);
-            Log.d("URL ", String.valueOf(Url));
-            urlConnection = (HttpURLConnection) url.openConnection();
-            //Log.d("URL ", String.valueOf(urlConnection));
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader bR = new BufferedReader(new InputStreamReader(in));
-            String line = "";
-
-            StringBuilder responseStrBuilder = new StringBuilder();
-            while ((line = bR.readLine()) != null) {
-
-                responseStrBuilder.append(line);
-            }
-            in.close();
-
-            JSONObject result = new JSONObject(responseStrBuilder.toString());
-
-            List<JSONObject> lst = getTravelDetails(result);
-            Log.d("ASRI", String.valueOf(lst));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            urlConnection.disconnect();
         }
-    }
 
 
 
-    }
-
-    public void buttonClick(View view) {
 
 
     }
 
+    public void showDetails(View view) {
+        TextView deptStop = findViewById(R.id.deptStop);
 
+
+    }
+
+    public static Map<String,List<String>> processingJSON(List<JSONObject> jsonObjectList){
+        Map<String,List<String>> map = new HashMap<>();
+        String destinationName,destinationArrivalTime,sourceName,sourceDepartureTime,totalTime,headSign;
+        String arrivalStop,arrivalTime, busName, busNumber, busDetails;
+        for(int i=0;i<jsonObjectList.size();i++){
+            JSONObject object = jsonObjectList.get(i);
+            try {
+                arrivalStop = object.getJSONObject("transit_details").getJSONObject("arrival_stop").getString("name");
+                arrivalTime = object.getJSONObject("transit_details").getJSONObject("arrival_time").getString("text");
+                totalTime = object.getJSONObject("duration").getString("text");
+                destinationName = object.getJSONObject("transit_details").getJSONObject("departure_stop").getString("name");
+                destinationArrivalTime = object.getJSONObject("transit_details").getJSONObject("departure_time").getString("text");
+                headSign = object.getJSONObject("transit_details").getString("headsign");
+                busName = object.getJSONObject("transit_details").getJSONObject("line").getJSONObject("vehicle").getString("name");
+                busNumber = object.getJSONObject("transit_details").getJSONObject("line").getString("short_name");
+                busDetails = busName+busNumber;
+
+                if(!map.containsKey("arrivalStop")){
+                    map.put("arrivalStop",new ArrayList<String>());
+                }
+                map.get("arrivalStop").add(arrivalStop);
+
+                if(!map.containsKey("arrivalTime")){
+                    map.put("arrivalTime",new ArrayList<String>());
+                }
+                map.get("arrivalTime").add(arrivalTime);
+
+                if(!map.containsKey("totalTime")){
+                    map.put("totalTime",new ArrayList<String>());
+                }
+                map.get("totalTime").add(totalTime);
+
+                if(!map.containsKey("destinationName")){
+                    map.put("destinationName",new ArrayList<String>());
+                }
+                map.get("destinationName").add(destinationName);
+
+                if(!map.containsKey("destinationArrivalTime")){
+                    map.put("destinationArrivalTime",new ArrayList<String>());
+                }
+                map.get("destinationArrivalTime").add(destinationArrivalTime);
+
+                if(!map.containsKey("headSign")){
+                    map.put("headSign",new ArrayList<String>());
+                }
+                map.get("headSign").add(headSign);
+
+                if(!map.containsKey("busDetails")){
+                    map.put("busDetails",new ArrayList<String>());
+                }
+                map.get("busDetails").add(busDetails);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
+    }
     public static List<JSONObject> getTravelDetails(JSONObject jsonObject){
         List<JSONObject> result = new ArrayList<>();
         try{
